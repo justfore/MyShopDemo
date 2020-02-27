96d5da28a8150e44d8fade3383dea6e6617d2d74
justfore <31989057+justfore@users.noreply.github.com>
2020-02-27 14:00:56 +0100

    Entity added

---
+++ MyShop/MyShop.DataAccess.SQL/Migrations/202002261849188_Initial.cs
@@ -0,0 +1,43 @@
+﻿namespace MyShop.DataAccess.SQL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.ProductCategories",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Description = c.String(),
+                        Category = c.String(),
+                        CreatedAt = c.DateTimeOffset(nullable: false, precision: 7),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Products",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Name = c.String(maxLength: 20),
+                        Price = c.Double(nullable: false),
+                        Description = c.String(),
+                        Category = c.String(),
+                        Image = c.String(),
+                        CreatedAt = c.DateTimeOffset(nullable: false, precision: 7),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Products");
+            DropTable("dbo.ProductCategories");
+        }
+    }
+}
